2d61b9424fa6436046ab00e131b2ee9782183592
xaeolus <xaeolus0769@gmail.com>
2023-05-19 14:32:45 +0800

    Changes to be committed: 	modified:   "\357\274\255ain.py"
    Changes not staged for commit:
    	deleted:    "\357\274\255ain.py"
    
    Untracked files:
    	main.py

--- Ｍain.py
+++ Ｍain.py
@@ -1,9 +1,11 @@
+import time as t
+
 # 计算两个坐标之间的距离
 def get_distance(addr1, addr2):
     x1, y1 = addr1
     x2, y2 = addr2
     distance = (((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5)
-    print(f"地址{addr1}和{addr2}之间的距离是{distance}吉米") 
+    #print(f"地址{addr1}和{addr2}之间的距离是{distance}吉米") 
     return distance
 
 # 拉格朗日的坐标为(X,Y),每一小格(基地、前哨所在单位)在坐标上再细分为10，如(3066, 4081)可以理解为(306.6, 408.1)。
@@ -15,78 +17,53 @@ def get_placement(begin_addr, target_addr):
     distance = [get_distance(begin_addr, target_addr),get_distance(begin_addr, angle[0]),get_distance(begin_addr, angle[1]),get_distance(begin_addr, angle[2]),get_distance(begin_addr, angle[3])] #定义5数值列表，用于存放起飞点到目标点和目标点4个角的距离
     
     #获取distance中最小值的索引，即为曲率落点。
-    return target_addr,distance.index(min(distance[1:4]))    #返回目标所在小格左下角坐标和曲率落点角的索引，1为左上角，2为左下角，3为右上角，4为右下角。  
+    down_point_index = distance.index(min(distance[1:4]))   #返回目标所在小格左下角坐标和曲率落点角的索引，1为左上角，2为左下角，3为右上角，4为右下角。
+    down_point_addr = angle[down_point_index-1]             #获取曲率落点的坐标
+    print(f"地址{begin_addr}到{target_addr}的曲率落点是{angle[distance.index(min(distance[1:4]))-1]}")
+    print(f"曲率落点所在小格的左下角坐标为{angle[1]},落点角为{distance.index(min(distance[1:4]))}。(注：1为左上角，2为左下角，3为右上角，4为右下角)")
+    #return angle[1],distance.index(min(distance[1:4]))      #返回目标所在小格左下角坐标和曲率落点角的索引，1为左上角，2为左下角，3为右上角，4为右下角。  
         
+def get_outpost_addr(begin_addr, target_addr):    #通过设置前哨曲率坐标的X或者Y获取前哨曲率坐标
+    x1, y1 = begin_addr
+    x2, y2 = target_addr
+       
+    k = (y2 - y1) / (x2 - x1)   #斜率k = (y2 - y1)/(x2 - x1)
+    b = y1 - k * x1             #斜率公式 y = kx + b, b = y - kx
+
+    outpost_addr_x = 0  #前哨曲率坐标的X
+    outpost_addr_y = 0  #前哨曲率坐标的Y
+
+    while outpost_addr_x == 0 and outpost_addr_y == 0 :
+        outpost_addr_x = int(input("请输入前哨的X坐标: "))
+        
+        while outpost_addr_x != 0 and outpost_addr_y == 0 :
+            outpost_addr_y = k * outpost_addr_x + b
+            return outpost_addr_x,outpost_addr_y
+        
+        utpost_addr_xy = int(input("请输入前哨的Y坐标: "))
+        while outpost_addr_x == 0 and outpost_addr_y != 0 :
+            outpost_addr_x = (outpost_addr_y - b)/k
+            return outpost_addr_x,outpost_addr_y    
+
+def get_nei_time(distance,speed):
+    return (distance * 10000)/speed/60
+
 begin_addr = (3094, 4063) #起飞坐标
 target_addr = (1975,1455) #目标坐标
+base_addr = (0,0)           #设置基地坐标
 
-print(get_placement(begin_addr, target_addr))
+get_placement(begin_addr, get_outpost_addr(begin_addr, target_addr)) #打印前哨设置的信息。
 
-exit()
+distance = get_distance(begin_addr,target_addr)
+speed = 2500
 
-#2. 根据斜率求直线方程:
-def get_line_equation(Begin, Target, x = 0, y = 0):
-    x1, y1 = Begin
-    x2, y2 = Target
-       
-    k = (y2 - y1) / (x2 - x1) #斜率k = (y2 - y1)/(x2 - x1)
-    b = y1 - k * x1 #斜率公式 y = kx + b, b = y - kx
-
-    if x == 0 :
-        if y != 0 :
-            x = (y - b)/k
-            return int(x),y
-    elif y == 0 :
-        y = k * x + b
-        return x,y
-    else :
-        return x,y
-        
-
-# EndAddr = get_line_equation(BeginAddr,TargetAddr,int(input("请输入前哨的X坐标: ")),int(input("请输入前哨的Y坐标: ")))
-EndAddr = get_line_equation(BeginAddr,TargetAddr,0,3182)
-print(EndAddr)
-
-print(2500/(719.98/(49)))
-print(3250/(719.98/(38)))
-
-print((462.79*172)/2500)
-print((462.79*172)/3250)
-
-exit()
-# 3. 判定点在直线上方、下方还是在直线上:
-def point_position(addr, k, b):
-    x, y = addr
-    if y == k * x + b:
-        return '在直线上'
-    elif y > k * x + b:
-        return '在直线上方'
-    else:
-        return '在直线下方'
-    
-print(point_position(TigerAddr, k, b))  # 在直线上方
-
-
-# 4. 求直线与坐标轴交点:
-def intersect_point(k, b):
-    if k == 0:
-        x = 0
-        y = b
-    elif k == float('inf'):
-        x = b 
-        y = 0
-    else:
-        x = -b / k
-        y = 0
-    return x, y
-
-x, y = intersect_point(2, 8)  # x = -4, y = 0
-
-exit()
-# 获取用户输入的两个地址坐标
-addr1 = int(input("请输入第一个地址的x坐标: ")), int(input("请输入第一个地址的y坐标: "))
-addr2 = int(input("请输入第二个地址的x坐标: ")), int(input("请输入第二个地址的y坐标: "))
-
-# 计算用户输入地址之间的距离
-distance = get_distance(addr1, addr2)
-print(f"您输入的两个地址{addr1}和{addr2}之间的距离是{distance}吉米")#print("hello world")
+neit_time = get_nei_time(distance,speed)
+print(f"地址{begin_addr}到{target_addr}的距离是{distance}吉米，速度是{speed}米/分钟，需要{neit_time}分钟。")
+#将一个表示分钟的浮点数转为时间格式，如138.5分钟转为2小时18分钟.公式为：小时=整数(分钟/60),分钟=余数(分钟/60)。编程语言中的整数函数为int()，余数函数为%。即
+#print(f"{int(neit_time/60)}小时{int(neit_time%60)}分钟")
+#设定一个到达时间，一个需要时间，从而计算出发时间。如到达时间为12:00，需要时间为2小时18分钟，则出发时间为9:42。具体计算公式为：出发时间=到达时间-需要时间。编程语言中的时间函数为time()，时间格式化函数为strftime()。
+print(t.strftime("%H:%M", t.localtime(t.mktime(t.strptime("2013-05-19 12:00","%Y-%m-%d %H:%M")) - neit_time * 60)))
+#strftime()函数的第一个参数是时间格式，第二个参数是时间戳。时间格式中的%H表示小时，%M表示分钟。localtime()函数的参数是时间戳，返回值是一个时间元组。time()函数的返回值是一个时间戳。
+#strftime函数在那个库中：
+#怎样将时间转换为时间戳：如12:00转换为720分钟，即12*60=720。编程语言中的时间函数为time()，时间格式化函数为strftime()。time()函数的返回值是一个时间戳。时间戳是从1970年1月1日0时0分0秒到现在的总秒数。
+#怎样将2013-05-19 12:20:00转换为时间戳：如2013-05-19 12:20:00转换为时间戳，编程为time.mktime(time.strptime("2013-05-19 12:20:00","%Y-%m-%d %H:%M:%S"))。strptime()函数的第一个参数是时间字符串，第二个参数是时间格式。mktime()函数的参数是时间元组，返回值是一个时间戳。
