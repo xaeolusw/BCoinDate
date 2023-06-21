542726422e61b942adb2c0765d74377d8005fda8
Jinhui Xie <xaeolus0769@gmail.com>
2023-06-21 17:19:41 +0800

    modified:   getKlines.py

--- getKlines.py
+++ getKlines.py
@@ -1,32 +1,102 @@
 import pandas as pd
 import ccxt
 import time
-import os
+import os,sys
 from datetime import timedelta, datetime
 
-pd.set_option('expand_frame_repr', False)  # 当列太多时不换行
-
 # =====设定参数
+#定义全局变量类
 proxies = {
     'http': 'http://127.0.0.1:7890',
     'https': 'http://127.0.0.1:7890'
 }
 
-binance = ccxt.binance({
+global_binance_exchange = ccxt.binance({
     'proxies': proxies
 })
 
-okex = ccxt.okex5({
+global_okex_exchange = ccxt.okex5({
     'proxies': proxies
 })
+    
+global_database_path = ''
+global_file_path = ''
 
+if os.name == 'nt':
+    global_database_path = r'D:\PythonProjects\getKlinesDatabase.csv'
+    global_file_path = r'D:\PythonProjects\coin_data'
+elif os.name == 'posix':
+    global_database_path = r'/Volumes/USB-DISK/getKlinesDatabase.csv'
+    global_file_path = r'/Volumes/USB-DISK/PythonProjects/coin_data'
+else:
+    print('操作系统不支持')
+    exit()
+
+pd.set_option('expand_frame_repr', False)  # 当列太多时不换行
+df = pd.read_csv(
+    filepath_or_buffer = global_database_path, #MAC系统下的路径r'/Volumes/USB-DISK/getKlinesDatabase.csv';Windows系统下的路径r'D:\PythonProjects\getKlinesDatabase.csv'
+    encoding='utf8', 
+    sep=',',
+)  # 从csv文件中读取数据
+
+global_error_list = []
+global_time_interval_list = []  # 其他可以尝试的值：'1m', '5m', '15m', '30m', '1H', '2H', '1D', '1W', '1M', '1Y'
+
+global_binance_symbol_list = []  #'BTCUSDT','ETHUSDT','EOSUSDT','LTCUSDT'
+
+global_okx_symbol_list = []  #'BTCUSDT','ETHUSDT','EOSUSDT','LTCUSDT'
+global_okx_instType_list = ['SWAP','FUTURES'] #SPOT：币币;SWAP：永续合约;FUTURES：交割合约;OPTION：期权
+global_okx_uly_list = ['BTC-USDT','BTC-USD']
+
+for symblo in df['symbol']:
+    global_binance_symbol_list.append(symblo + 'USDT')
+    global_okx_symbol_list.append(symblo + '-USDT')
+     
+for interval in df['interval']:
+    if interval != '0':
+        global_time_interval_list.append(interval)
+
+global_start_time = df['start_time'][0]
+global_end_time = df['end_time'][0]
+global_update_time = df['update_time'][0]
+global_instType = 'SPOT'
+# getPreDayDate = True
+# start_time = '2023-06-16 00:00:00'
+# end_time = '2023-06-18 23:59:00'
+
+def save_data_to_csv(day, df, exchange):
+    global global_file_path, global_instType
+    # 创建交易所文件夹
+    file_path = os.path.join(global_file_path, exchange.id)
+    if os.path.exists(file_path) is False:
+        os.mkdir(file_path)
+    # 创建spot文件夹
+    file_path = os.path.join(file_path, global_instType)
+    if os.path.exists(file_path) is False:
+        os.mkdir(file_path)
+    # 创建交易对文件夹
+    file_path = os.path.join(file_path, symbol)
+    if os.path.exists(file_path) is False:
+        os.mkdir(file_path)
+    # 创建频次文件夹
+    file_path = os.path.join(file_path, time_interval)
+    if os.path.exists(file_path) is False:
+        os.mkdir(file_path)
+
+    # 拼接文件目录
+    file_name = '_'.join([exchange.name, symbol.replace('/', '-'), str(pd.to_datetime(day)).split(' ')[0].replace('-',''),time_interval]) + '.csv'
+    file_path = os.path.join(file_path, file_name)
+
+    print(file_path)
+    df.to_csv(file_path, index=False)
 
 def get_binance_klines(symbol, time_interval, start_time, end_time):
-    start_time_since = binance.parse8601(start_time) #parse8601（），用于将 ISO 8601 格式的时间字符串转换为 Unix 时间戳
-    end_time_since = binance.parse8601(end_time)
+    global global_binance_exchange, global_instType
+    start_time_since = global_binance_exchange.parse8601(start_time) #parse8601（），用于将 ISO 8601 格式的时间字符串转换为 Unix 时间戳
+    end_time_since = global_binance_exchange.parse8601(end_time)
 
     # =====循环获取数据
-    df_list = []
+    # df_list = pd.DataFrame()
     all_kline_data = []
 
     while True:
@@ -38,12 +108,12 @@ def get_binance_klines(symbol, time_interval, start_time, end_time):
             'limit': 1000,
         }  
 
-        kline_data = binance.publicGetKlines(params=params)
+        kline_data = global_binance_exchange.publicGetKlines(params=params,_self=global_binance_exchange)  # 获取数据
 
-        df = pd.DataFrame(kline_data, dtype=float)  # 将数据转换为dataframe #缺少这两行代码
-        df_list.append(df)  #缺少这两行代码
+        # df = pd.DataFrame(kline_data, dtype=float)  # 将数据转换为dataframe #缺少这两行代码
+        # df_list = pd.concat([df], ignore_index=True)  #缺少这两行代码
       
-        if df.shape[0] > 1:
+        if len(kline_data) > 1:
             start_time_since = kline_data[-1][0]  # 更新since，为下次循环做准备
             all_kline_data += kline_data
         else:
@@ -53,7 +123,8 @@ def get_binance_klines(symbol, time_interval, start_time, end_time):
         time.sleep(2)
 
     # 对数据进行整理
-    df = pd.concat(df_list, ignore_index=True)
+    # df = pd.concat(all_kline_data, ignore_index=True)
+    df = pd.DataFrame(all_kline_data, dtype=float) 
 
     if df.shape[0] > 0:
         df.rename(columns={0: 'MTS', 1: 'open', 2: 'high', 3: 'low', 4: 'close', 5: 'volume',6:'kline_close_time',7:'quote_asset_volume',8:'number_of_trades',9:'taker_buy_base_asset_volume',10:'taker_buy_quote_asset_volume'}, inplace=True)
@@ -68,39 +139,37 @@ def get_binance_klines(symbol, time_interval, start_time, end_time):
     
             # =====保存数据到文件
             if df.shape[0] > 0:
-                # 根目录，确保该路径存在
-                path = r'D:\PythonProjects\coin_data'
-                #path = path.encode('utf-8')#.decode('utf-8')  # 处理中文乱码问题
-                #MAC系统下的路径:'/Volumes/USB-DISK/PythonProjects/coin_data'
-                #Windows系统下的路径:'D:\PythonProjects\coin_data'
+                save_data_to_csv(day, df, global_binance_exchange)
+                # # 创建交易所文件夹
+                # file_path = os.path.join(global_file_path, global_binance_exchange.id)
+                # if os.path.exists(file_path) is False:
+                #     os.mkdir(file_path)
+                # # 创建spot文件夹
+                # file_path = os.path.join(file_path, 'spot')
+                # if os.path.exists(file_path) is False:
+                #     os.mkdir(file_path)
+                # # 创建交易对文件夹
+                # file_path = os.path.join(file_path, symbol)
+                # if os.path.exists(file_path) is False:
+                #     os.mkdir(file_path)
+                # # 创建频次文件夹
+                # file_path = os.path.join(file_path, time_interval)
+                # if os.path.exists(file_path) is False:
+                #     os.mkdir(file_path)
 
-                # 创建交易所文件夹
-                path = os.path.join(path, binance.id)
-                if os.path.exists(path) is False:
-                    os.mkdir(path)
-                # 创建spot文件夹
-                path = os.path.join(path, 'spot')
-                if os.path.exists(path) is False:
-                    os.mkdir(path)
-                # 创建交易对文件夹
-                path = os.path.join(path, symbol)
-                if os.path.exists(path) is False:
-                    os.mkdir(path)
-                # 创建频次文件夹
-                path = os.path.join(path, time_interval)
-                if os.path.exists(path) is False:
-                    os.mkdir(path)
+                # # 拼接文件目录
+                # file_name = '_'.join([global_binance_exchange.name, symbol.replace('/', '-'), str(pd.to_datetime(day)).split(' ')[0].replace('-',''),time_interval]) + '.csv'
+                # file_path = os.path.join(file_path, file_name)
 
-                # 拼接文件目录
-                file_name = '_'.join([binance.name, symbol.replace('/', '-'), str(pd.to_datetime(day)).split(' ')[0].replace('-',''),time_interval]) + '.csv'
-                path = os.path.join(path, file_name)
-
-                print(path)
-                df.to_csv(path, index=False)
+                # print(file_path)
+                # df.to_csv(file_path, index=False)
    
 def get_okex_klines(symbol, time_interval, start_time, end_time):
-    start_time_since = okex.parse8601(start_time) -1000 #parse8601（），用于将 ISO 8601 格式的时间字符串转换为 Unix 时间戳
-    end_time_since = okex.parse8601(end_time)
+    global global_okex_exchange, global_instType
+    start_time_since = global_okex_exchange.parse8601(start_time)
+    if start_time_since != None:
+        start_time_since -= 1000 #parse8601（），用于将 ISO 8601 格式的时间字符串转换为 Unix 时间戳
+    end_time_since = global_okex_exchange.parse8601(end_time)
     # timestamp = pd.to_datetime(start_time_since, unit='ms')
     # print(timestamp)
     # timestamp = pd.to_datetime(end_time_since, unit='ms')
@@ -118,7 +187,7 @@ def get_okex_klines(symbol, time_interval, start_time, end_time):
             'before': start_time_since, #请求此时间戳之后（更新的数据）的分页内容            
         }  
 
-        kline_data = okex.publicGetMarketHistoryCandles(params=params)['data']
+        kline_data = global_okex_exchange.publicGetMarketHistoryCandles(params=params,_self=global_okex_exchange)['data']
         # for i in range(len(kline_data)):
         #     print(pd.to_datetime(int(kline_data[i][0]), unit='ms'))
         
@@ -149,93 +218,73 @@ def get_okex_klines(symbol, time_interval, start_time, end_time):
    
         # =====保存数据到文件
         if df.shape[0] > 0:
+            save_data_to_csv(day, df, global_okex_exchange)
             # 根目录，确保该路径存在
-            path = r'D:\PythonProjects\coin_data'
+            #path = r'/Volumes/USB-DISK/PythonProjects/coin_data'
             #MAC系统下的路径:'/Volumes/USB-DISK/PythonProjects/coin_data'
             #Windows系统下的路径:'D:\PythonProjects\coin_data'
 
             # 创建交易所文件夹
-            path = os.path.join(path, okex.id)
-            if os.path.exists(path) is False:
-                os.mkdir(path)
-            # 创建spot文件夹
-            path = os.path.join(path, instType)
-            if os.path.exists(path) is False:
-                os.mkdir(path)
-            # 创建交易对文件夹
-            path = os.path.join(path, symbol)
-            if os.path.exists(path) is False:
-                os.mkdir(path)
-            # 创建频次文件夹
-            path = os.path.join(path, time_interval)
-            if os.path.exists(path) is False:
-                os.mkdir(path)
+            # filePath = os.path.join(global_file_path, global_okex_exchange.id)
+            # if os.path.exists(filePath) is False:
+            #     os.mkdir(filePath)
+            # # 创建spot文件夹
+            # filePath = os.path.join(filePath, global_instType)
+            # if os.path.exists(filePath) is False:
+            #     os.mkdir(filePath)
+            # # 创建交易对文件夹
+            # filePath = os.path.join(filePath, symbol)
+            # if os.path.exists(filePath) is False:
+            #     os.mkdir(filePath)
+            # # 创建频次文件夹
+            # filePath = os.path.join(filePath, time_interval)
+            # if os.path.exists(filePath) is False:
+            #     os.mkdir(filePath)
 
-            # 拼接文件目录
-            file_name = '_'.join([okex.name, symbol.replace('/', '-'), str(pd.to_datetime(day)).split(' ')[0].replace('-',''),time_interval]) + '.csv'
-            path = os.path.join(path, file_name)
+            # # 拼接文件目录
+            # file_name = '_'.join([global_okex_exchange.name, symbol.replace('/', '-'), str(pd.to_datetime(day)).split(' ')[0].replace('-',''),time_interval]) + '.csv'
+            # filePath = os.path.join(filePath, file_name)
 
-            print(path)
-            df.to_csv(path, index=False)
+            # print(filePath)
+            # df.to_csv(filePath, index=False)
 
+pre_pre_day = (datetime.now() - timedelta(days=2)).strftime('%Y-%m-%d')
+if pre_pre_day == global_update_time:
+    print('更新昨天数据中')
+    global_update_time = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+    getPreDayDate = True
+else:
+    print('批量更新数据中')
+    getPreDayDate = False
 
-# =====设定参数
-df = pd.read_csv(
-    filepath_or_buffer = r'D:\PythonProjects\getKlinesDatabase.csv', #MAC系统下的路径r'/Volumes/USB-DISK/getKlinesDatabase.csv';Windows系统下的路径r'D:\PythonProjects\getKlinesDatabase.csv'
-    encoding='utf8', 
-    sep=',',
-)  # 从csv文件中读取数据
+#print(pre_pre_day,global_update_time,getPreDayDate)
 
-error_list = []
-binance_symbol_list = []  #'BTCUSDT','ETHUSDT','EOSUSDT','LTCUSDT'
-okx_symbol_list = []  #'BTCUSDT','ETHUSDT','EOSUSDT','LTCUSDT'
-time_interval_list = []  # 其他可以尝试的值：'1m', '5m', '15m', '30m', '1H', '2H', '1D', '1W', '1M', '1Y'
-
-for symblo in df['symbol']:
-    binance_symbol_list.append(symblo + 'USDT')
-    okx_symbol_list.append(symblo + '-USDT')
-
-for interval in df['interval']:
-    if interval != '0':
-        time_interval_list.append(interval)
-
-start_time = df['start_time'][0]
-end_time = df['end_time'][0]
-# print(binance_symbol_list,okx_symbol_list,time_interval_list)
-# print(df['start_time'][0],df['end_time'][0])
-# exit()
-
-
-instType = 'SPOT'
-getPreDayDate = True
-# start_time = '2023-06-16 00:00:00'
-# end_time = '2023-06-18 23:59:00'
-
+#程序开始：
 if getPreDayDate:
     # =====每天抓取数据
-    end_time = datetime.now() - timedelta(days=1) #days=1代表前一天，days=0代表当天,如此类推
-    start_time = end_time.strftime("%Y-%m-%d") + ' 00:00:00'
-    end_time = end_time.strftime("%Y-%m-%d") + ' 23:59:00'
+    start_time = df['update_time'][0] + ' 00:00:00'
+    end_time = df['update_time'][0] + ' 23:59:00'
+    global_instType = 'spot'
     print(f'获取binance{start_time} 至 {end_time}数据')
 
-    for symbol in binance_symbol_list:
-        for time_interval in time_interval_list:
+    for symbol in global_binance_symbol_list:
+        for time_interval in global_time_interval_list:
             try:
                 get_binance_klines(symbol, time_interval, start_time, end_time)
             except Exception as e:
                 print(e)
-                error_list.append('_'.join([binance.id, symbol, time_interval]))
+                global_error_list.append('_'.join([str(global_binance_exchange.id), symbol, time_interval]))
 
     # =====获取okx前一天币币数据
     print(f'获取okx{start_time} 至 {end_time}币币数据')
-     
-    for symbol in okx_symbol_list:
-        for time_interval in time_interval_list:
+    global_instType = 'SPOT' 
+    for symbol in global_okx_symbol_list:
+        for time_interval in global_time_interval_list:
             try:
                 get_okex_klines(symbol, time_interval, start_time, end_time)
             except Exception as e:
                 print(e)
-                error_list.append('_'.join([okex.id, symbol, time_interval]))
+                global_error_list.append('_'.join([str(global_okex_exchange.id), symbol, time_interval]))
 
     # =====抓取数据开始结束时间
     print(f'获取okx{start_time} 至 {end_time}合约数据')
@@ -245,6 +294,7 @@ if getPreDayDate:
     uly_list = ['BTC-USDT','BTC-USD']
     for instType in instType_list:
         symbol_list = []
+        global_instType = instType
 
         for uly in uly_list:
             params = {
@@ -253,63 +303,73 @@ if getPreDayDate:
             #'instFamily': 'BTC-USD', #交易品种.适用于交割/永续/期权，如 BTC-USD          
             } 
 
-            tickers = okex.publicGetMarketTickers(params=params)['data']
+            tickers = global_okex_exchange.publicGetMarketTickers(params=params, _self=global_okex_exchange)['data']
             for ticker in tickers:
                 symbol_list.append(ticker['instId'])
             #print(symbol_list)
 
         for symbol in symbol_list:
-            for time_interval in time_interval_list:
+            for time_interval in global_time_interval_list:
                 get_okex_klines(symbol, time_interval, start_time, end_time)
 else:
     #=====选择开始、结束时间抓取数据
-    binance_start_time = start_time
-    binance_end_time = end_time
+    binance_start_time = (datetime.strptime(global_update_time, '%Y-%m-%d') + timedelta(days=1)).strftime('%Y-%m-%d') + ' 00:00:00'
+    # global_update_time = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+    global_update_time = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+    binance_end_time = global_update_time + ' 23:59:00'
+
+    okx_start_time = binance_start_time
+    okx_end_time = binance_end_time
+    if binance_start_time > binance_end_time:
+        print('开始时间大于结束时间，不进行操作！')
+        sys.exit(0)
+    
+    print(f'抓取{binance_start_time}到{binance_end_time}的数据！')
 
     while binance_start_time < binance_end_time :
         temp_time = str(pd.to_datetime(binance_end_time) - timedelta(days=30))
 
         if temp_time > binance_start_time :
-            for symbol in binance_symbol_list:
-                for time_interval in time_interval_list:
+            for symbol in global_binance_symbol_list:
+                for time_interval in global_time_interval_list:
                     try:
                         get_binance_klines(symbol, time_interval, temp_time, binance_end_time)
                     except Exception as e:
                         print(e)
-                        error_list.append([symbol, time_interval, temp_time, binance_end_time])
+                        global_error_list.append([symbol, time_interval, temp_time, binance_end_time])
             binance_end_time = temp_time
         else:
-            for symbol in binance_symbol_list:
-                for time_interval in time_interval_list:
+            for symbol in global_binance_symbol_list:
+                for time_interval in global_time_interval_list:
                     try:
                         get_binance_klines(symbol, time_interval, binance_start_time, binance_end_time)
                     except Exception as e:
                         print(e)
-                        error_list.append([symbol, time_interval, binance_start_time, binance_end_time])
+                        global_error_list.append([symbol, time_interval, binance_start_time, binance_end_time])
             break
 
     # =====选择开始、结束时间抓取数据
-    okx_start_time = start_time
-    okx_end_time = end_time
     while okx_start_time < okx_end_time :
         temp_time = str(pd.to_datetime(okx_end_time) - timedelta(days=1))
 
         if temp_time > okx_start_time :
-            for symbol in okx_symbol_list:
-                for time_interval in time_interval_list:
+            for symbol in global_okx_symbol_list:
+                for time_interval in global_time_interval_list:
                     get_okex_klines(symbol, time_interval, temp_time, okx_end_time)
             okx_end_time = temp_time
         else:
-            for symbol in okx_symbol_list:
-                for time_interval in time_interval_list:
+            for symbol in global_okx_symbol_list:
+                for time_interval in global_time_interval_list:
                     get_okex_klines(symbol, time_interval, okx_start_time, okx_end_time)
             break
 
-if len(error_list) > 0:
+if len(global_error_list) > 0:
     print('以下数据抓取失败：')
-    print(error_list)
+    for error in global_error_list:
+        print(error)
 else:
     print('数据抓取成功！')
-    df['start_time'][0] = end_time
-    df['end_time'][0] = end_time
-    df.to_csv(r'D:\PythonProjects\getKlinesDatabase.csv', index=False)
+    # df['start_time'][0] = global_update_time
+    # df['end_time'][0] = global_update_time
+    df['update_time'][0] = global_update_time
+    df.to_csv(global_database_path, index=False)
