1652ca3a2e5e1f715fb8b4b32c8905335ec870a3
Jinhui Xie <xaeolus0769@gmail.com>
2023-06-29 21:58:13 +0800

    modified:   "7_\346\237\245\347\234\213\347\255\226\347\225\245\347\273\223\346\236\234.py" 	modified:   __pycache__/Evaluate.cpython-311.pyc 	modified:   __pycache__/Position.cpython-311.pyc 	modified:   __pycache__/Signals.cpython-311.pyc 	modified:   data/equity_curve.csv

--- 7_查看策略结果.py
+++ 7_查看策略结果.py
@@ -20,30 +20,30 @@ drop_days = 10  # 币种刚刚上线10天内不交易
 
 
 # =====读入数据
-df = pd.read_hdf('/Volumes/USB-DISK/PythonProjects/coin_data/binance_%s.h5' % symbol, key='BTCUSDT_5m')
-# 任何原始数据读入都进行一下排序、去重，以防万一
-# df = df[df['candle_begin_time'] >= pd.to_datetime('2023-01-01')]
-df.sort_values(by=['candle_begin_time'], inplace=True)
-df.drop_duplicates(subset=['candle_begin_time'], inplace=True)
-df.reset_index(inplace=True, drop=True)
+df = pd.read_hdf('D:\\PythonProjects\\BCoinDate\\data\\binance_BTCUSDT_5m.h5', key='BTCUSDT_15m')
+# # 任何原始数据读入都进行一下排序、去重，以防万一
+# # df = df[df['candle_begin_time'] >= pd.to_datetime('2023-01-01')]
+# df.sort_values(by=['candle_begin_time'], inplace=True)
+# df.drop_duplicates(subset=['candle_begin_time'], inplace=True)
+# df.reset_index(inplace=True, drop=True)
 
 
-# =====转换为其他分钟数据
-rule_type = '15T'
-period_df = df.resample(rule=rule_type, on='candle_begin_time', label='left', closed='left').agg(
-    {'open': 'first',
-     'high': 'max',
-     'low': 'min',
-     'close': 'last',
-     'volume': 'sum',
-     'quote_asset_volume': 'sum',
-     })
-period_df.dropna(subset=['open'], inplace=True)  # 去除一天都没有交易的周期
-period_df = period_df[period_df['volume'] > 0]  # 去除成交量为0的交易周期
-period_df.reset_index(inplace=True)
-df = period_df[['candle_begin_time', 'open', 'high', 'low', 'close', 'volume', 'quote_asset_volume']]
-df = df[df['candle_begin_time'] >= pd.to_datetime('2017-01-01')]
-df.reset_index(inplace=True, drop=True)
+# # =====转换为其他分钟数据
+# rule_type = '15T'
+# period_df = df.resample(rule=rule_type, on='candle_begin_time', label='left', closed='left').agg(
+#     {'open': 'first',
+#      'high': 'max',
+#      'low': 'min',
+#      'close': 'last',
+#      'volume': 'sum',
+#      'quote_asset_volume': 'sum',
+#      })
+# period_df.dropna(subset=['open'], inplace=True)  # 去除一天都没有交易的周期
+# period_df = period_df[period_df['volume'] > 0]  # 去除成交量为0的交易周期
+# period_df.reset_index(inplace=True)
+# df = period_df[['candle_begin_time', 'open', 'high', 'low', 'close', 'volume', 'quote_asset_volume']]
+# df = df[df['candle_begin_time'] >= pd.to_datetime('2017-01-01')]
+# df.reset_index(inplace=True, drop=True)
 
 listDf = pd.DataFrame(["para", "equity_curve"])
 # =====计算交易信号
